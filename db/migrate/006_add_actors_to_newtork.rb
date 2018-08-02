bb187a812a45300314b93ee7b0c2a554a6bf7470
Yurina Shin <yurinas@gmail.com>
2018-08-02 14:28:38 +0000

    automatically backed up by learn

--- db/migrate/006_add_actors_to_newtork.rb
+++
@@ -1,8 +0,0 @@
-class AddActorsToNetwork < ActiveRecord::Migration[4.2]
-  #to make an additive change to a schema we create
-  #a new migration, and then in the change method,
-  #we make the change.
-  def change
-    add_column :actor_id, :integer
-  end
-end
