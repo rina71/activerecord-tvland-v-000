536a245047e64bee20acb191f435753c8da3ed12
Yurina Shin <yurinas@gmail.com>
2018-08-02 15:06:44 +0000

    automatically backed up by learn

--- db/migrate/008_add_colums_to_show.rb
+++ db/migrate/008_add_colums_to_show.rb
@@ -1,6 +1,6 @@
 class AddColumnsToShow < ActiveRecord::Migration[4.2]
 
   def change
-    add_column :characters, :catchphrase, :string
+    add_column :show, :day, :string, :season, :string
   end
 end
