4952be8de589d1e565f3086b38f3cb23a99e0db0
Yurina Shin <yurinas@gmail.com>
2018-08-02 14:46:17 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,7 +3,6 @@ class Actor < ActiveRecord::Base
   has_many :shows, through: :characters
 
   def full_name
-    binding.pry
-    self.first_name
+    [self.first_name, self.last_name].join(' ')
   end
 end
